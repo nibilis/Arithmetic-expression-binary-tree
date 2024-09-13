7e46c919b22af63f50b33e97cf6ba69045f299a7
0Akuro0 <103699767+0Akuro0@users.noreply.github.com>
2024-09-12 21:13:11 -0300

    Update Program.java
    Adicionei uma forma de transformar a expressão em posfixa (podia ser melhor mas funciona), e comecei a tentar aplicar isso pra adicionar direitinho na arvore

--- application/Program.java
+++ application/Program.java
@@ -10,38 +10,28 @@
 package application;
 
 import util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Program
 {
     public static void main(String[] args){
-        BinaryTree tree = new BinaryTree();
-
-        OperatorNode n1 = new OperatorNode('*');
-        OperatorNode n2 = new OperatorNode('+');
-        OperatorNode n3 = new OperatorNode('/');
-        OperatedNode n4 = new OperatedNode(3.0f);
-        OperatedNode n5 = new OperatedNode(9.0f);
-        OperatedNode n6 = new OperatedNode(6.0f);
-        OperatedNode n7 = new OperatedNode(7.0f);
-
-        n1.setLeft(n2);
-        n1.setRight(n7);
-
-        n2.setRight(n6);
-        n2.setLeft(n3);
-
-        n3.setLeft(n5);
-        n3.setRight(n4);
-
-
-        tree.setRoot(n1);
-
-        tree.inOrderTraversal();
-        System.out.println();
-        tree.postOrderTraversal();
-        System.out.println();
-
-        System.out.println(tree.solve());
+        //creating binary tree
+    	BinaryTree tree = new BinaryTree();
+        //receiving expression
+        Scanner sc = new Scanner(System.in);
+        String infix = sc.nextLine();
+        String postfix = infixToPostfix(infix);
+        System.out.println(postfix); //TODO remove this
+        //validating expression
+        //TODO
+        //using the infix expression to create the tree`s nodes
+        int size = postfix.length();
+        OperatorNode root = new OperatorNode(postfix.charAt(size-2));
+        System.out.println(postfix.charAt(size-2));
+        createTree(root, size-2, postfix);
+        System.out.println(root.getData());
+        System.out.println(root.getLeft());
     }
 
     //Method that receives no parameter and has
@@ -57,4 +47,68 @@ public class Program
         sb.append("5. Exit program\n");
         System.out.print(sb.toString());
     }
+    
+    //turning the infix expression to postfix
+    public static String infixToPostfix(String infix) {
+    	//this will only work if the given expression has already been verified
+    	String number = "";
+    	String postfix = "";
+    	Stack<Character> stack = new Stack<Character>();
+    	
+    	for(int i = 0; i < infix.length(); i++) { // check each character in the String
+    		if(isOperator(infix.charAt(i))) { // if it`s an operator, add the numbers before it to the postfix variable (this only works if the expression is written correctly)
+    			postfix+=number;
+    			postfix+=" ";
+    			number="";
+    			if(stack.isEmpty() == false) { // check for the previous operators found in the String
+    				if(priorityCheck(stack.peek()) > priorityCheck(infix.charAt(i))) { // if the one at the top has a lower priority, add the stack to the postfix variable and then add the new operator to the empty stack
+    					while(stack.empty() == false) {
+    						postfix += stack.pop();
+    						postfix += " ";
+    					}
+    				}
+    			}
+    			stack.push(infix.charAt(i));
+    		}
+    		else {
+    			number+=infix.charAt(i); //if its not an operator then it`s a number. Doing like this will separate different numbers
+    		}
+    	}
+    	//adding the remaining numbers and operators:
+    	postfix+=number + " ";
+		while(stack.empty() == false) {
+			postfix += stack.pop();
+			postfix += " ";
+		}
+		
+    	return postfix;
+
+    }
+    public static int priorityCheck(char check) { //see the "priority level" of the character
+		switch (check) {
+			case '+':
+			case '-':
+				return 1;
+			case '*':
+			case '/':
+				return 2;
+			case '^':
+			    return 3;
+			default:
+			    return 0;
+		}
+	}
+    public static boolean isOperator(char verify) {
+    	return verify=='+' || verify=='-'||verify=='*'||verify=='/';
+    }
+    public static void createTree(BaseNode<?> father, int index, String postfix) {
+    	if(isOperator(postfix.charAt(index-2))) {
+    		System.out.println("operator");
+    		if(father.getLeft() == null) {
+    			OperatorNode node = new OperatorNode(postfix.charAt(index-2));
+    			father.setLeft(node);
+    		}
+    	}
+    }
 }
+
