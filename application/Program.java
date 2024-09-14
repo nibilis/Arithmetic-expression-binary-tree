f9ce87d0968993ecd32206bb02778150856a4abb
Nicolas Henriques de Almeida <Nhenriquesdealmeida@gmail.com>
2024-09-14 02:36:27 -0300

    Creation of the insert method on BinaryTree class
    Also added tests on Program.java

--- application/Program.java
+++ application/Program.java
@@ -6,6 +6,7 @@
 // References:
 // - https://docs.oracle.com/javase/8/docs/api/java/lang/StringBuilder.html
 // - https://docs.oracle.com/javase/7/docs/api/java/lang/Float.html
+// - https://raj457036.github.io/Simple-Tools/prefixAndPostfixConvertor.html
 
 package application;
 
@@ -16,32 +17,26 @@ public class Program
     public static void main(String[] args){
         BinaryTree tree = new BinaryTree();
 
-        OperatorNode n1 = new OperatorNode('*');
-        OperatorNode n2 = new OperatorNode('+');
-        OperatorNode n3 = new OperatorNode('/');
+        MultiplicationNode n1 = new MultiplicationNode();
+        SumNode n2 = new SumNode();
+        DivisionNode n3 = new DivisionNode();
         OperatedNode n4 = new OperatedNode(3.0f);
         OperatedNode n5 = new OperatedNode(9.0f);
         OperatedNode n6 = new OperatedNode(6.0f);
         OperatedNode n7 = new OperatedNode(7.0f);
 
-        n1.setLeft(n2);
-        n1.setRight(n7);
 
-        n2.setRight(n6);
-        n2.setLeft(n3);
-
-        n3.setLeft(n5);
-        n3.setRight(n4);
-
-
-        tree.setRoot(n1);
+        tree.insert(n3);
+        tree.insert(n4);
+        tree.insert(n1);
+        tree.insert(n6);
+        tree.insert(n2);
+        tree.insert(n5);
+        tree.insert(n7);
 
         tree.inOrderTraversal();
-        System.out.println();
-        tree.postOrderTraversal();
-        System.out.println();
-
         System.out.println(tree.solve());
+
     }
 
     //Method that receives no parameter and has
