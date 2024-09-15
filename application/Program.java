52a4ca56fa80d6c1c532b69f4bcfe05fe9fa2651
0Akuro0 <103699767+0Akuro0@users.noreply.github.com>
2024-09-15 00:39:49 -0300

    Update Program.java

--- application/Program.java
+++ application/Program.java
@@ -10,33 +10,65 @@
 
 package application;
 
+import java.util.List;
+
+import java.util.Stack;
+
+import java.util.Scanner;
+
 import util.*;
+
 
 public class Program
 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Expression: ");
+        String input = sc.nextLine();
+    	
+        tokenizer tkz = new tokenizer(input);
+        
+        List<String> tokens = tkz.tokenize();
+
+        System.out.println("---------- toPostfix ------------");
+        Stack<String> postfix = new Stack<>();
+        postfix = infixToPostfix(tokens);
+        System.out.println(postfix);
+        System.out.println("-------------- TO TREE -----------");
         BinaryTree tree = new BinaryTree();
-
-        MultiplicationNode n1 = new MultiplicationNode();
-        SumNode n2 = new SumNode();
-        DivisionNode n3 = new DivisionNode();
-        OperatedNode n4 = new OperatedNode(3.0f);
-        OperatedNode n5 = new OperatedNode(9.0f);
-        OperatedNode n6 = new OperatedNode(6.0f);
-        OperatedNode n7 = new OperatedNode(7.0f);
-
-
-        tree.insert(n3);
-        tree.insert(n4);
-        tree.insert(n1);
-        tree.insert(n6);
-        tree.insert(n2);
-        tree.insert(n5);
-        tree.insert(n7);
-
+        int size = postfix.size();
+        for(int i = 0; i < size; i++) {
+        	String current = postfix.pop();
+        	char currValue = current.charAt(0);
+        	switch(currValue) {
+        	case '+':
+        		SumNode sumN = new SumNode();
+        		tree.insert(sumN);
+        		break;
+        	case '-':
+        		SubtractionNode subN = new SubtractionNode();
+        		tree.insert(subN);
+        		break;
+        	case '*':
+        		MultiplicationNode mulN = new MultiplicationNode();
+        		tree.insert(mulN);
+        		break;
+        	case '/':
+        		DivisionNode divN = new DivisionNode();
+        		tree.insert(divN);
+        		break;
+        	default:
+        		Float result = Float.valueOf(current);
+        		OperatedNode opN = new OperatedNode(result);
+        		tree.insert(opN);
+        		break;
+        	}
+        }
+        System.out.print("In order traversal: ");
         tree.inOrderTraversal();
+        System.out.println();
         System.out.println(tree.solve());
-
+        
     }
 
     //Method that receives no parameter and has
@@ -52,4 +84,44 @@ public class Program
         sb.append("5. Exit program\n");
         System.out.print(sb.toString());
     }
+    
+   // infix to postfix
+    
+   
+   public static Stack<String> infixToPostfix(List<String> tokens) {
+	   Stack<String> postfix = new Stack<>();
+	   Stack<String> operators = new Stack<>();
+	   for(int i = 0; i < tokens.size(); i++) {
+       		if(Character.isDigit(tokens.get(i).charAt(0))) {
+       			postfix.push(tokens.get(i));
+       		}
+       		else {
+       			if(operators.isEmpty() == false) {
+       				if(priorityCheck(operators.peek().charAt(0)) > priorityCheck(tokens.get(i).charAt(0))) {
+       					while(operators.isEmpty() == false) {
+       						postfix.push(operators.pop());
+       					}
+       				}
+       			}
+       			operators.push(tokens.get(i));
+       		}
+       }
+	   while(operators.isEmpty() == false) {
+		   postfix.push(operators.pop());
+	   }
+	   return postfix;
+   }
+   public static int priorityCheck(char check) { //see the "priority level" of the character
+		switch (check) {
+			case '+':
+			case '-':
+				return 1;
+			case '*':
+			case '/':
+				return 2;
+			default:
+			    return 0;
+		}
+	}
+   
 }
